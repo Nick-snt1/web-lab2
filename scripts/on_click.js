5f3f15920fe61d8e0aef2a5f2b16a2bc2c87d04e
Nick-snt1 <gumangi3579@gmail.com>
2022-10-11 00:08:44 +0300

    Write script

--- scripts/on_click.js
+++ scripts/on_click.js
@@ -1,3 +1,37 @@
 function onlyOne(checkbox) {
     document.getElementsByName('r').forEach(item => { if (item !== checkbox) item.checked = false })
+    
+    $('.checkbox:checked').is(":checked") ? $('.a').removeClass('invalid') : $('.a').addClass('invalid');
+
 }
+
+
+$(function () {
+
+    function validate() {
+        return $(".radio:checked").length === 1 && document.getElementById("y-field").validity.valid && $('.checkbox:checked').is(":checked"); 
+    }
+
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        if (!validate()) return;
+        $.ajax({
+            url: 'php/main.php',
+            method: 'GET',
+            data: $(this).serialize() + '&timezone=' + new Date().getTimezoneOffset(),
+            dataType: "json",
+            beforeSend: () => $('.button').attr('disabled', 'disabled'),
+            success: data => {
+                $('.button').attr('disabled', false);
+                if (data.validate) 
+                    $('#result-table').append('<tr>'
+                    + '<td>' + data.x + '</td>'
+                    + '<td>' + data.y + '</td>'
+                    + '<td>' + data.r + '</td>'
+                    + '<td>' + data.curtime + '</td>'
+                    + '<td>' + data.exectime + '</td>'
+                    + '<td>' + data.hitres + '</td>');    
+            }
+        });
+    });
+});
